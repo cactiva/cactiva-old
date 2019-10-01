a3eff9e6b58b061fdc75799456d82528159e29f2
IrulArif <masterirul@gmail.com>
2019-10-01 15:48:25 +0700

    fix

---
+++ src/components/editor/tags/TouchableOpacity/element.tsx
@@ -0,0 +1,34 @@
+import { observer, useObservable } from 'mobx-react-lite';
+import React from 'react';
+import ErrorBoundary from 'react-error-boundary';
+import CactivaDraggable from '../../CactivaDraggable';
+import CactivaDroppable from '../../CactivaDroppable';
+import CactivaSelectable from '../../CactivaSelectable';
+import { parseProps } from '../../utility/parser';
+import { renderChildren } from '../../utility/renderchild';
+
+export default observer((props: any) => {
+  const cactiva = props._cactiva;
+  const tagProps = parseProps(props);
+  const meta = useObservable({ dropOver: false });
+  delete tagProps.onPress;
+  return (
+    <ErrorBoundary>
+      <CactivaDraggable cactiva={cactiva}>
+        <CactivaDroppable
+          cactiva={cactiva}
+          onDropOver={(value: boolean) => (meta.dropOver = value)}
+        >
+          <CactivaSelectable cactiva={cactiva}>
+            <div {...tagProps}>
+              <div
+                className={`cactiva-drop-after ${meta.dropOver && 'hover'}`}
+              />
+              {renderChildren(cactiva.source, cactiva.editor, cactiva.root)}
+            </div>
+          </CactivaSelectable>
+        </CactivaDroppable>
+      </CactivaDraggable>
+    </ErrorBoundary>
+  );
+});
