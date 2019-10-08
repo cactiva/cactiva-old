ca989c4917261f9064c5196573fb3c0ddd8b8513
rizkyramadhan <rizky@andromedia.co.id>
2019-10-08 23:29:30 +0700

    fix

--- src/components/ctree/CactivaTree.tsx
+++ src/components/ctree/CactivaTree.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import useAsyncEffect from "use-async-effect";
 import api from "@src/libs/api";
 import "./CactivaTree.scss";
-import { Text, Icon } from "evergreen-ui";
+import { Text, Icon, SearchInput } from "evergreen-ui";
 import CactivaDraggable from "../editor/CactivaDraggable";
 import { SyntaxKind } from "../editor/utility/syntaxkinds";
 import tags from "../editor/utility/tags";
@@ -18,38 +18,49 @@ export default observer(({ editor }: any) => {
 
   return (
     <div className="cactiva-tree">
-      {_.sortBy(meta.list, "type").map((e: any, i: number) => {
-        const el = (
-          <>
-            <div className="icon">
-              {e.type === "dir" ? (
-                <Icon icon="folder-close" size={10} color="#66788A" />
-              ) : (
-                <Icon icon="code" size={10} color="#66788A" />
-              )}
+      <SearchInput
+        className="search"
+        placeholder="Search"
+        width="100%"
+        height={25}
+        spellCheck={false}
+      />
+      <div className="list">
+        {_.sortBy(meta.list, "type").map((e: any, i: number) => {
+          const name =
+            e.type === "dir" ? e.name : e.name.substr(0, e.name.length - 4);
+          const el = (
+            <>
+              <div className="icon">
+                {e.type === "dir" ? (
+                  <Icon icon="folder-close" size={10} color="#66788a" />
+                ) : (
+                  <Icon icon="code" size={10} color="#66788a" />
+                )}
+              </div>
+              <Text color="#333">{name}</Text>
+            </>
+          );
+          return e.type === "dir" ? (
+            <div key={i} className="item">
+              {el}
             </div>
-            <Text>{e.name}</Text>
-          </>
-        );
-        return e.type === "dir" ? (
-          <div key={i} className="item">
-            {el}
-          </div>
-        ) : (
-          <CactivaDraggable
-            key={i}
-            cactiva={{
-              source: { id: null },
-              tag: {
-                tagName: e.name,
-                mode: "component"
-              }
-            }}
-          >
-            <div className="item">{el}</div>
-          </CactivaDraggable>
-        );
-      })}
+          ) : (
+            <CactivaDraggable
+              key={i}
+              cactiva={{
+                source: { id: null },
+                tag: {
+                  tagName: name,
+                  mode: "component"
+                }
+              }}
+            >
+              <div className="item">{el}</div>
+            </CactivaDraggable>
+          );
+        })}
+      </div>
     </div>
   );
 });
