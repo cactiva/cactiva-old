f477d1ab4acdbdd12bdc8a006b52a6b1ad0b7e48
rizkyramadhan <rizky@andromedia.co.id>
2019-10-09 18:29:04 +0700

    fix

--- src/components/editor/CactivaEditor.tsx
+++ src/components/editor/CactivaEditor.tsx
@@ -1,30 +1,31 @@
-import api from '@src/libs/api';
-import { observer, useObservable } from 'mobx-react-lite';
-import React, { useCallback, useEffect } from 'react';
-import { useDropzone } from 'react-dropzone';
-import CactivaBreadcrumb from './CactivaBreadcrumb';
-import CactivaToolbar from './CactivaToolbar';
-import './editor.scss';
-import './tags/tags.scss';
+import api from "@src/libs/api";
+import _ from "lodash";
+import { observer, useObservable } from "mobx-react-lite";
+import React, { useCallback, useEffect } from "react";
+import { useDropzone } from "react-dropzone";
+import MonacoEditor from "react-monaco-editor";
+import Split from "react-split";
+import CactivaBreadcrumb from "./CactivaBreadcrumb";
+import CactivaToolbar from "./CactivaToolbar";
+import "./editor.scss";
+import "./tags/tags.scss";
 import {
   addChildInId,
   commitChanges,
   createNewElement,
   insertAfterElementId,
-  prepareChanges,
-  findElementById
-} from './utility/elements/tools';
-import { renderChildren } from './utility/renderchild';
-import { SyntaxKind } from './utility/syntaxkinds';
-import MonacoEditor from 'react-monaco-editor';
-import { generateSource } from './utility/parser/generateSource';
-import _ from 'lodash';
+  prepareChanges
+} from "./utility/elements/tools";
+import { generateSource } from "./utility/parser/generateSource";
+import { renderChildren } from "./utility/renderchild";
+import { SyntaxKind } from "./utility/syntaxkinds";
+
 const uploadImage = async (file: any) => {
   var formDataToUpload = new FormData();
-  formDataToUpload.append('file', file);
-  return await api.post('/assets/upload', formDataToUpload, {
+  formDataToUpload.append("file", file);
+  return await api.post("/assets/upload", formDataToUpload, {
     headers: {
-      'Content-Type': 'multipart/form-data'
+      "Content-Type": "multipart/form-data"
     }
   });
 };
@@ -33,26 +34,26 @@ export default observer(({ source, editor }: any) => {
   const meta = useObservable({
     onDrag: false,
     jsx: false,
-    source: ''
+    source: ""
   });
   const children = renderChildren(
-    { name: '--root--', children: [source] },
+    { name: "--root--", children: [source] },
     editor
   );
 
   const onDrop = useCallback(async acceptedFiles => {
-    if (!acceptedFiles[0].type.includes('image/')) {
-      alert('Images only!');
+    if (!acceptedFiles[0].type.includes("image/")) {
+      alert("Images only!");
       return;
     }
     prepareChanges(editor);
-    const el = createNewElement('Image');
+    const el = createNewElement("Image");
     const file: any = await uploadImage(acceptedFiles[0]);
-    el.props['source'] = {
+    el.props["source"] = {
       kind: SyntaxKind.CallExpression,
       value: `require('@src/assets/images/${file.filename}')`
     };
-    el.props['style'] = {
+    el.props["style"] = {
       kind: SyntaxKind.ObjectLiteralExpression,
       value: {
         width: {
@@ -79,37 +80,54 @@ export default observer(({ source, editor }: any) => {
   delete rootProps.onClick;
 
   useEffect(() => {
-    meta.source = _.get(editor, 'selected.source')
+    meta.source = _.get(editor, "selected.source")
       ? generateSource(editor.selected.source)
-      : '';
+      : "";
   }, [editor.selectedId]);
   return (
     <div className="cactiva-editor" {...rootProps}>
       {meta.onDrag && <input {...getInputProps()} />}
+
       <div className="cactiva-wrapper">
         <CactivaToolbar />
-        <div className="cactiva-canvas">{children}</div>
-      </div>
-      {meta.jsx && (
-        <div className="cactiva-editor-source">
-          <MonacoEditor
-            theme="vs-dark"
-            value={meta.source}
-            onChange={value => {
-              console.log(value);
-              meta.source = value;
-            }}
-            editorWillMount={monaco => {
-              editor.setupMonaco(monaco);
-            }}
-            language="typescript"
-          />
+        <div className="cactiva-editor-wrapper">
+          <Split
+            sizes={meta.jsx ? [50, 50] : [100]}
+            gutterSize={5}
+            gutterAlign="center"
+            dragInterval={1}
+            direction="vertical"
+            className={`cactiva-editor-content ${
+              meta.jsx ? "split" : "unsplit"
+            }`}
+          >
+            <div className="cactiva-canvas">{children}</div>
+            <div
+              className={`cactiva-editor-source `}
+              style={{ display: meta.jsx ? "flex" : "none" }}
+            >
+              {meta.jsx && (
+                <MonacoEditor
+                  theme="vs-dark"
+                  value={meta.source}
+                  onChange={value => {
+                    console.log(value);
+                    meta.source = value;
+                  }}
+                  editorWillMount={monaco => {
+                    editor.setupMonaco(monaco);
+                  }}
+                  language="typescript"
+                />
+              )}
+            </div>
+          </Split>
         </div>
-      )}
+      </div>
       <div className="cactiva-editor-footer">
         <CactivaBreadcrumb source={source} editor={editor} />
         <div
-          className={`footer-icon ${meta.jsx ? 'active' : ''}`}
+          className={`footer-icon ${meta.jsx ? "active" : ""}`}
           onClick={() => {
             meta.jsx = !meta.jsx;
           }}
